514a00bc1706548917bf564cbb8311af1b6e8fe1
wanting.zhang <wanting.zhang@ele.me>
2016-02-28 16:33:02 +0800

    update js, remove useless css

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,116 +1,4 @@
 /***************** Waypoints ******************/
-
-$(document).ready(function() {
-
-     $(document).ready(function(){
-          $.get("blogs",function(data,status){
-            $("#intro .container").html(
-              function () {
-                var result='';
-                var postList = data;
-                for(var index in postList) {
-                  if(JSON.stringify(postList[index]).length > 10) {
-                    result+='<div class="row">';
-                    result+='<div class="col-md-8 col-md-offset-2 wp1">';
-                    result+='<h1 class="arrow">'+postList[index].title+'</h1>';
-                    result+='<p class="time">@'+postList[index].author+' '+postList[index].time+'</p>';
-                    if(postList[index].image == null || postList[index].image == "") {
-                      result+='<p class="text-left font-color-black">'+postList[index].content+'</p>';
-                    } else {
-                      result+='<div id="servicesSlider"><ul class="slides">';
-                      result+='<li><div class="post-pic"><img src="'+postList[index].image+'"/></div</li>';
-                      result+='<li>'+postList[index].content+'</li>';
-                      result+='</ul></div>';
-                    }
-                    result+='</div>';
-                    result+='</div>';
-                    if(postList.length != ++index) {
-                       result+='<hr>';
-                    }
-                  }
-                }
-                return result;
-              }
-            );
-          });
-
-          $.get("json/photo.json",function(data,status){
-            $("#portfolioSlider .slides").html(
-              function () {
-                var result='';
-                var photoHtml;
-                var photoList = data;
-                for(var index in photoList) {
-                  // var pli = document.createElement("li");
-                  if(JSON.stringify(photoList[index]).length > 10) {
-                    if(index%3 == 0) {
-                       photoHtml='<li>';
-                       photoHtml+='<div class="col-md-4 wp4">';
-                    } else if(index%3 == 1) {
-                       photoHtml='<div class="col-md-4 wp4 delay-05s">';
-                    } else {
-                       photoHtml='<div class="col-md-4 wp4 delay-1s">';
-                    }
-
-                    photoHtml+='<div class="overlay-effect effects clearfix">';
-                    photoHtml+='<div class="img">';
-                    photoHtml+='<img src="'+photoList[index].image+'" alt="Portfolio Item">';
-                    photoHtml+='<div class="overlay">';
-                    photoHtml+='<a href="#" class="expand"><i class="fa fa-search"></i><br>View More</a>';
-                    photoHtml+='<a class="close-overlay hidden">x</a>';
-                    photoHtml+='</div>';
-                    photoHtml+='</div>';
-                    photoHtml+='</div>';
-                    photoHtml+='<h2>'+photoList[index].title+'</h2>';
-                    photoHtml+='<p>'+photoList[index].content+'</p>';
-                    photoHtml+='</div>';
-
-                    if(index%3 == 2) {
-                      photoHtml+='</li>';
-                    }
-                    result+=photoHtml;
-                  }
-                }
-                return result;
-              }
-            );
-          });
-
-        });
-
-    $('.wp1').waypoint(function() {
-        $('.wp1').addClass('animated fadeInLeft');
-    }, {
-        offset: '75%'
-    });
-    $('.wp2').waypoint(function() {
-        $('.wp2').addClass('animated fadeInUp');
-    }, {
-        offset: '75%'
-    });
-    $('.wp3').waypoint(function() {
-        $('.wp3').addClass('animated fadeInDown');
-    }, {
-        offset: '55%'
-    });
-    $('.wp4').waypoint(function() {
-        $('.wp4').addClass('animated fadeInDown');
-    }, {
-        offset: '75%'
-    });
-    $('.wp5').waypoint(function() {
-        $('.wp5').addClass('animated fadeInUp');
-    }, {
-        offset: '75%'
-    });
-    $('.wp6').waypoint(function() {
-        $('.wp6').addClass('animated fadeInDown');
-    }, {
-        offset: '75%'
-    });
-
-});
-
 /***************** Slide-In Nav ******************/
 
 $(window).load(function() {
@@ -149,7 +37,7 @@ document.querySelector("#nav-toggle").addEventListener("click", function() {
 
 /***************** Overlays ******************/
 
-$(document).ready(function(){
+var setOverlays=function(){
     if (Modernizr.touch) {
         // show the close overlay button
         $(".close-overlay").removeClass("hidden");
@@ -177,13 +65,13 @@ $(document).ready(function(){
             $(this).removeClass("hover");
         });
     }
-});
+};
 
 /***************** Flexsliders ******************/
 
-$(window).load(function() {
+var setFlexsliders=function() {
 
-	$('#portfolioSlider').flexslider({
+	$('#photosSlider').flexslider({
 		animation: "slide",
 		directionNav: false,
 		controlNav: true,
@@ -194,7 +82,7 @@ $(window).load(function() {
 		}
 	});
 
-	$('#servicesSlider').flexslider({
+	$('#blogsSlider').flexslider({
 		animation: "slide",
 		directionNav: false,
 		controlNav: true,
@@ -204,16 +92,118 @@ $(window).load(function() {
 			$.waypoints('refresh');
 		}
 	});
+};
 
-	$('#teamSlider').flexslider({
-		animation: "slide",
-		directionNav: false,
-		controlNav: true,
-		touch: true,
-		pauseOnHover: true,
-		start: function() {
-			$.waypoints('refresh');
-		}
-	});
+var setClass = function() {
+    $('.wp1').waypoint(function() {
+    $('.wp1').addClass('animated fadeInLeft');
+    }, {
+        offset: '75%'
+    });
+    $('.wp2').waypoint(function() {
+        $('.wp2').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp3').waypoint(function() {
+        $('.wp3').addClass('animated fadeInDown');
+    }, {
+        offset: '55%'
+    });
+    $('.wp4').waypoint(function() {
+        $('.wp4').addClass('animated fadeInDown');
+    }, {
+        offset: '75%'
+    });
+    $('.wp5').waypoint(function() {
+        $('.wp5').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp6').waypoint(function() {
+        $('.wp6').addClass('animated fadeInDown');
+    }, {
+        offset: '75%'
+    });
+};
+
+
+$(document).ready(function() {
+
+  $.get("blogs",function(data,status){
+    $("#blogs .container").html(
+      function () {
+        var result='';
+        var blogList = data;
+        for(var index in blogList) {
+          if(JSON.stringify(blogList[index]).length > 10) {
+            result+='<div class="row">';
+            result+='<div class="col-md-8 col-md-offset-2 wp1">';
+            result+='<h1 class="arrow">'+blogList[index].title+'</h1>';
+            result+='<p class="time">@'+blogList[index].author+' '+blogList[index].time+'</p>';
+            if(blogList[index].image == null || blogList[index].image == "") {
+              result+='<p class="text-left font-color-black">'+blogList[index].content+'</p>';
+            } else {
+              result+='<div id="blogsSlider"><ul class="slides">';
+              result+='<li><div class="blog-pic"><img src="'+blogList[index].image+'"/></div</li>';
+              result+='<li>'+blogList[index].content+'</li>';
+              result+='</ul></div>';
+            }
+            result+='</div>';
+            result+='</div>';
+            if(blogList.length != ++index) {
+               result+='<hr>';
+            }
+          }
+        }
+        return result;
+      }
+    );
+    setClass();
+  });
+
+
+
+  $.get("json/photo.json",function(data,status){
+    $("#photosSlider .slides").html(
+      function () {
+        var result='';
+        var photoHtml;
+        var photoList = data;
+        for(var index in photoList) {
+          // var pli = document.createElement("li");
+          if(JSON.stringify(photoList[index]).length > 10) {
+            if(index%3 == 0) {
+                photoHtml='<li>';
+            } else {
+                photoHtml='';
+            }
+            photoHtml+='<div class="col-md-4 wp4">';
+            photoHtml+='<div class="overlay-effect effects clearfix">';
+            photoHtml+='<div class="img">';
+            photoHtml+='<img src="'+photoList[index].image+'" alt="Photos Item">';
+            photoHtml+='<div class="overlay">';
+            photoHtml+='<a href="#" class="expand"><i class="fa fa-search"></i><br>View More</a>';
+            photoHtml+='<a class="close-overlay hidden">x</a>';
+            photoHtml+='</div>';
+            photoHtml+='</div>';
+            photoHtml+='</div>';
+            photoHtml+='<h2>'+photoList[index].title+'</h2>';
+            photoHtml+='<p>'+photoList[index].content+'</p>';
+            photoHtml+='</div>';
+
+            if(index%3 == 2) {
+              photoHtml+='</li>';
+            }
+            result+=photoHtml;
+          }
+        }
+        return result;
+      }
+    );
+     setClass();
+     setOverlays();
+     setFlexsliders();
+  });
 
 });
